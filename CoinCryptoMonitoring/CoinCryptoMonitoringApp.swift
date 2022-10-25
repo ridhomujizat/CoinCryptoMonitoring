54b7028b26f9a9cd175776d5bd4e9c21aa25b069
ridhotitipku <107243155+ridhotitipku@users.noreply.github.com>
2022-10-25 18:10:55 +0700

    Initial Commit

---
+++ CoinCryptoMonitoring/CoinCryptoMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinCryptoMonitoringApp.swift
+//  CoinCryptoMonitoring
+//
+//  Created by ridho mujizat on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct CoinCryptoMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinCryptoMonitoring/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoinCryptoMonitoring
+//
+//  Created by ridho mujizat on 25/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
